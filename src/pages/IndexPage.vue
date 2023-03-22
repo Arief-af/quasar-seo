c642eaf35f891f2e8dbbb45d3fb0e7893d3b6df5
Arief-Af <you@example.com>
2023-03-22 18:03:33 +0700

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+
+// some .vue file
+import { useMeta } from 'quasar'
+
+const metaData = {
+  // sets document title
+  title: 'Kanaina Page',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: title => `${title} - Kanaina a good food from the earth`,
+
+  // meta tags
+  meta: {
+    description: { name: 'description', content: 'Kanaina' },
+    keywords: { name: 'Kanaina', content: 'Kanaina a good food from the earth' },
+    equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    // note: for Open Graph type metadata you will need to use SSR, to ensure page is rendered by the server
+    ogTitle:  {
+      property: 'og:title',
+      // optional; similar to titleTemplate, but allows templating with other meta properties
+      template (ogTitle) {
+        return `${ogTitle} - My Website`
+      }
+    }
+  },
+
+  // CSS tags
+  link: {
+    material: { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+  },
+
+  // JS tags
+  script: {
+    ldJson: {
+      type: 'application/ld+json',
+      innerHTML: `{ "@context": "http://schema.org" }`
+    }
+  },
+
+  // <html> attributes
+  htmlAttr: {
+    'xmlns:cc': 'http://creativecommons.org/ns#', // generates <html xmlns:cc="http://creativecommons.org/ns#">,
+    empty: undefined // generates <html empty>
+  },
+
+  // <body> attributes
+  bodyAttr: {
+    'action-scope': 'xyz', // generates <body action-scope="xyz">
+    empty: undefined // generates <body empty>
+  },
+
+  // <noscript> tags
+  noscript: {
+    default: 'This is content for browsers with no JS (or disabled JS)'
+  }
+}
+
+export default {
+  setup () {
+    // needs to be called in setup()
+    useMeta(metaData)
+  }
+}
+</script>
